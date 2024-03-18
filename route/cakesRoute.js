8e5ab37c10474af20448f2997dc98fc2b063ce95
Dani Chertov <chertov005@gmail.com>
2024-03-18 05:04:51 +0200

    3

--- route/cakesRoute.js
+++ route/cakesRoute.js
@@ -53,50 +53,78 @@ const {CakesModel,validCakes} = require('../DataBase/model/cakesModel')
 // });
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-router.get('/' , async(req , res) => {
+// router.get('/' , async(req , res) => {
 
-    //http://127.0.0.1:3000/cakes?perPage=3&page=5
+//     //http://127.0.0.1:3000/cakes?perPage=3&page=5
+//     let perPage = req.query.perPage ? req.query.perPage : 5;
+//     let page = req.query.page ? req.query.page-1 : 0;
+//     let myFilter = {} ;
+//     let s = req.query.s ;
+//     if(s) {
+//         let searchExp = new RegExp(s)
+//         myFilter = {$or:[{name:searchExp},{info:searchExp}]}
+//     }
 
-    try {
 
+//     try {
 
-        let perPage = req.query.perPage ? req.query.perPage : 5
-        let page = req.query.page ? req.query.page-1 : 0
-
-
-        let data = await CakesModel.find({}) .limit(perPage) .skip(perPage*page);
-        return res.status(200) .json(data);
+//         let data = await CakesModel.find(myFilter) .limit(perPage) .skip(perPage*page);
+//         return res.status(200) .json(data);
         
         
-    } catch (error) {
-        return res.status(500) .json({message:'internal server error 500'})
-    }
+//     } catch (error) {
+//         return res.status(500) .json({message:'internal server error 500'})
+//     }
     
     
-});
+// });
 
 
-router.get('/search' , async(req , res) => {
+
+
+
+
+// router.get('/search' , async(req , res) => {
     
-    try {
+//     try {
 
-        let s = req.query.s
-        let searchExp = new RegExp(s,'i')
+//         let s = req.query.s
+//         let searchExp = new RegExp(s,'i')
         
-        // let data = await CakesModel.find({name:searchExp})
-        let data = await CakesModel.find({$or:[{name:searchExp},{info:searchExp}]})
-        return res.status(200) .json(data);
+//         // let data = await CakesModel.find({name:searchExp})
+//         let data = await CakesModel.find({$or:[{name:searchExp},{info:searchExp}]})
+//         return res.status(200) .json(data);
 
-    } catch (error) {
-        return res.status(500) .json({message:'internal server error 500'})
+//     } catch (error) {
+//         return res.status(500) .json({message:'internal server error 500'})
 
-    }
+//     }
 
 
-});
+// });
 /////////////////////////////////////////////////////////////////////////////////////////////////
 
+router.get('/' ,async(req , res) => {
 
+    let perPage = req.query.perPage ?  req.query.perPage : 5 ;
+    let page = req.query.page ? req.query.page-1 : 0
+    let myFilter = {} ;
+    let s = req.query.s ;
+    if(s) {
+        let searchExp = new RegExp(s , 'i') 
+        myFilter = {$or:[{name:searchExp},{info:searchExp}]}
+    }
+
+    try {
+        
+        let data = await CakesModel.find(myFilter) .limit(perPage) .skip(perPage*page) ;
+        return res.status(200).json(data)
+
+    } catch (error) {
+        return res.status(500).json({message:'there some error with server , try later...'})
+    }
+
+});
 
 
 
