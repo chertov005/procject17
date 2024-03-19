8e34310679d6f1cfd756909d5ce18c674f014f2d
Dani Chertov <chertov005@gmail.com>
2024-03-19 12:33:17 +0200

    3

--- route/uploadRoute.js
+++ route/uploadRoute.js
@@ -1,49 +1,46 @@
 const express = require('express') ;
 const router = express.Router() ;
+const path = require('path');
 
 
-// router.post('/' , async(req , res) => {
-
-//     try {
-        
-//         let myFile = req.files.myFile
-
-//         myFile.mv(`public/image/${myFile.name}` , (err) => {
-//             return res.status(401).json(err)
-//         })
-
-//         return res.json({message:'success upload file'})
-
-
-
-//     } catch (error) {
-//         return res.status(500).json({message:'there error with server'})
-//     }
-
-// });
 
 /////////////////////////////////////////////////////////////////////////////////////////////'
 
 router.post('/' , async(req , res) => {
     
     try {
-    
-       let myFile = req.files.myFile ;
 
-       if(myFile.size <= 1024*1024*5) {
-        return myFile.mv(`public/image/${myFile.name}`,(err) => {
-            if(err) {
-                return res.json(err)
+        let myFile = req.files.myFile ;
+        if(myFile.size <= 1024*1024*5) {
+
+            let extsArray = ['.jpg','.jpeg','.jfif','.pjpeg','.pjp']
+            let checkExtsFile = path.extname(myFile.name)
+
+            if(extsArray.includes(checkExtsFile)) {
+                myFile.mv(`public/image/${myFile.name}` , (err) => {
+
+                    if(err) {
+                        return res.json(err)
+                    }
+    
+                    return res.json({message:'success upload file'})
+                    
+                })
+
             }
 
-            return res.json({message:'success file upload !!'})
-        })
-        
-       }
+            else{
+                res.json({message:'exts File not allow , only image '})
+            }
 
-       else {
-        res.json({message:'file over 2mb size error '})
-       }
+         
+        }
+        
+        else{
+            
+            return res.json({message:'file over 5MB , max you can upload 5MB'})
+        }
+       
 
     } catch (error) {
     return res.status(500).json({message:'there error with server'})
