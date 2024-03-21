74db34277dc78cd661997b3941f294ebb0fd7bb5
Dani Chertov <chertov005@gmail.com>
2024-03-21 13:36:12 +0200

    2

--- public/js/fileUpload.js
+++ public/js/fileUpload.js
@@ -2,57 +2,55 @@
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
-// window.addEventListener('load' , async () => {
 
+window.addEventListener('load' , () => {
 
-//     declareViewEvent()
-
-// });
-
-
-
-// const declareViewEvent = async() =>{
-
-//     document.querySelector('#id_form').addEventListener('submit' , async(e) => {
-
-//         e.preventDefault();
-
-//         let id_inp = document.querySelector('#id_inp');
-
-//         console.log(id_file.files[0])
-
-//         let formMyData = new FormData() ;
-
-//         formMyData.append('myFile' , id_inp.files[0])
-
-//         doApiUpload(formMyData)
-
-//     });
-
-// };
-
-
-
-
-// const doApiUpload = async(_body) => {
-
-//     let url = 'http://127.0.0.1:3000/upload' ;
-//     let resp = await fetch(url ,{
-//         method:'POST' ,
-//         body: _body
-//     })
-//     let data = await resp.json()
-
-//     console.log(data)
-// };
-
-///////////////////////////////////////////////////////////////////////////////////////////////////////
-
-
-
-window.addEventListener('load' , async() => {
-
+    declareViewEvent()
 
 });
 
 
+
+const diApiUpload = async(_body) => {
+
+    let url = 'http://127.0.0.1:3000/upload'
+    let resp = await fetch(url ,{
+        method:'POST' ,
+        body:_body
+    })  
+    let data = await resp.json()
+
+    console.log(data)
+
+};
+
+
+
+
+
+
+
+
+
+
+const declareViewEvent = () => {
+
+    let id_form = document.querySelector('#id_form') ;
+
+    id_form.addEventListener('submit' , async(e) => {
+
+        e.preventDefault() ;
+
+        let inp_file = document.querySelector('#inp_file').files[0]
+
+        let myDataForm = new FormData() ;
+        myDataForm.append("myFile" ,inp_file)
+
+
+
+        diApiUpload(myDataForm)
+        
+
+    });
+
+};
